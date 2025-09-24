36e53365ac424df96c9ae6652474b8e7f58878b2
gustavo costa <gustavocosta.miguel@estudante.ufjf.br>
2025-09-23 22:16:24 -0300

    final da aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Disciplina from './disciplina';
 
 function App() {
   return (<>
   <h1>Ola mundo</h1>
   <h2>Ola react</h2>
+  <Disciplina></Disciplina>
 </>);
     
 }
